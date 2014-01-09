95ce9c04b10d2ebc22bd46b8bd757cc02f76eb16
biwek <biweks@gmail.com>
2014-01-09 09:27:19 +0000

    added comments to directory.rb file

--- directory.rb
+++ directory.rb
@@ -1,10 +1,15 @@
+# prints headers
 puts "----------------"
 puts "List of students"
 puts "----------------"
+
+#prints list of students
 puts "Biwek Shrestha"
 puts "Nabin"
 puts "James"
 puts "Andrey"
+
+#prints the number of students
 print "Overall, there are "
 print "4"
 puts " students"
