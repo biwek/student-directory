54500ecc88b7688d15b15e6997da9f46f91c3437
biwek <biweks@gmail.com>
2014-01-09 12:21:00 +0000

    added student array and used iteration to go through the array

--- directory.rb
+++ directory.rb
@@ -1,15 +1,24 @@
+
+# an array of student names
+stu_names = [
+			"Biwek Shrestha",
+			"Nabin",
+			"James",
+			"Andrey"
+			]
+
 # prints headers
 puts "----------------"
 puts "List of students"
 puts "----------------"
 
-#prints list of students
-puts "Biwek Shrestha"
-puts "Nabin"
-puts "James"
-puts "Andrey"
+# use a loop
+stu_names.each do |name|
+	puts name
+end
+
 
 #prints the number of students
 print "Overall, there are "
-print "4"
+print stu_names.length
 puts " students"
