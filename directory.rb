7e3cbc116624c8bfd54ce5070db13a496c60acbf
biwek <biweks@gmail.com>
2014-01-09 12:30:40 +0000

    added methods

--- directory.rb
+++ directory.rb
@@ -8,15 +8,27 @@ stu_names = [
 			]
 
 # prints headers
-puts "----------------"
-puts "List of students"
-puts "----------------"
+def print_header
+	puts "----------------"
+	puts "List of students"
+	puts "----------------"
+end
 
 # using iteration
-stu_names.each do |name|
-	puts name
+def print_student_names(stu_list)
+	stu_list.each do |name|
+		puts name
+	end
 end
 
 
-#prints the number of students
-puts "Overall, there are #{stu_names.length} students"
+# prints the number of students
+def print_student_length(stu_list)
+	puts "Overall, there are #{stu_list.length} students"
+end
+
+# using the methods
+print_header
+print_student_names(stu_names)
+print_student_length(stu_names)
+
